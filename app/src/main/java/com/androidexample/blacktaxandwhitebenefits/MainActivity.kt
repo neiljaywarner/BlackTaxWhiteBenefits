a74140bbbca90e0d64c6ec444f57a8f70e53269b
brian.reinke <brian.reinke@reinkecorp.local>
2018-12-29 21:41:26 -0600

    017-proper widget sizing for various phones

--- app/src/main/java/com/androidexample/blacktaxandwhitebenefits/MainActivity.kt
+++ app/src/main/java/com/androidexample/blacktaxandwhitebenefits/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         initialize()
         setupListeners()
 
